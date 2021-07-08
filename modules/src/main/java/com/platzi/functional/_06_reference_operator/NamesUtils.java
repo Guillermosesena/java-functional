95af70c22235a9658d47eb5f3363026ed5de7a80
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-07 22:30:43 -0500

    16/39 Reference operator

---
+++ modules/src/main/java/com/platzi/functional/_06_reference_operator/NamesUtils.java
@@ -0,0 +1,23 @@
+package com.platzi.functional._06_reference_operator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class NamesUtils {
+
+    public static void main(String[] args) {
+        List<String> teachers = getList("Nicolas","Juan", "Zulema");
+        Consumer<String> printer = text-> System.out.println(text);
+        teachers.forEach(printer);
+        System.out.println("////////");
+        //:: -> find the method
+        teachers.forEach(System.out::println);
+    }
+
+
+    static <T> List<T> getList(T... elements){
+        return Arrays.asList(elements);
+    }
+}
