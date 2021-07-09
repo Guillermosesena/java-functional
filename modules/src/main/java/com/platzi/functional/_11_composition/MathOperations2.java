47242b53c8509d39049db2ef09f88a2efdf9024d
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-08 19:24:17 -0500

    21/39 Composition

---
+++ modules/src/main/java/com/platzi/functional/_11_composition/MathOperations2.java
@@ -0,0 +1,31 @@
+package com.platzi.functional._11_composition;
+
+import java.util.function.Function;
+
+public class MathOperations2 {
+    public static void main(String[] args) {
+        Function<Integer, Integer> multiplyBy3 = x->{
+            System.out.println("Multiplying x: " + x + " by 3");
+            return x *3;
+        };
+
+        Function<Integer, Integer> add1MultiplyBy3 =
+                multiplyBy3.compose(y ->{
+                    System.out.println("Adding 1 to: "+ y);
+                    return y+1;
+                });
+
+        Function<Integer, Integer> andSquare =
+                add1MultiplyBy3.andThen(x-> {
+                    System.out.println("It's raising " + x + " to the power of two");
+                    return  x * x;
+                });
+
+        System.out.println(
+                add1MultiplyBy3.apply(5)
+        );
+        System.out.println("/////");
+        System.out.println(andSquare.apply(3));
+
+    }
+}
